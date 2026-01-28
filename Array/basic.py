d2a293b1f16d7e5c09ccb259b8ae23fd74d94214
Nilesh Tamboli <nileshtamboli428@gmail.com>
2026-01-28 17:43:27 +0530

    Array

---
+++ Array/basic.py
@@ -0,0 +1,14 @@
+arr = [1,2,3,4,5]
+print(arr)
+# add element at the end
+arr.append(6)
+print(arr)
+# remove element from the end
+arr.pop()
+print(arr)
+# remove element from the start
+arr.pop(0)
+print(arr)
+# add element at the start
+arr.insert(0,1)
+print(arr)
